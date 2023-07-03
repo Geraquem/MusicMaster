224ecf67d3f391a3fa0ee791d8866155a0e078d7
Geraquem <geraquem@gmail.com>
2023-07-03 20:31:27 +0200

    wip

--- app/src/main/java/com/mmfsin/musicmaster/presentation/guesser/model/MusicVideoDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.mmfsin.musicmaster.presentation.guesser.model
-
-data class MusicVideoDTO(
-    val titulo : String = "",
-    val artista : String = "",
-    val year : String = "",
-    val url : String = "",
-)
