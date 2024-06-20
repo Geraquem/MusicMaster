c81a9f87640ec15e399d192583e974562a870472
gers <geraquem@gmail.com>
2024-06-20 12:28:46 +0200

    wip

--- app/src/main/java/com/mmfsin/musicmaster/domain/usecases/GetMusicDataUseCase.kt
+++ app/src/main/java/com/mmfsin/musicmaster/domain/usecases/GetMusicDataUseCase.kt
@@ -8,8 +8,24 @@ import javax.inject.Inject
 class GetMusicDataUseCase @Inject constructor(private val repository: IMusicRepository) :
     BaseUseCase<GetMusicDataUseCase.Params, List<Music>>() {
 
-    override suspend fun execute(params: Params) =
-        repository.getMusicDataFromFirebase(params.categoryId)
+    override suspend fun execute(params: Params): List<Music> {
+        val musicList = repository.getMusicDataFromFirebase(params.categoryId)
+
+        return try {
+            val random = System.currentTimeMillis().toString().last().toString().toInt()
+            val iteration = if (random == 0) 2
+            else if (random < 4) random
+            else 3
+
+            var randomList = listOf<Music>()
+            for (i in 1..iteration) {
+                randomList = musicList.shuffled()
+            }
+            randomList
+        } catch (e: Exception) {
+            musicList.shuffled()
+        }
+    }
 
     data class Params(
         val categoryId: String
