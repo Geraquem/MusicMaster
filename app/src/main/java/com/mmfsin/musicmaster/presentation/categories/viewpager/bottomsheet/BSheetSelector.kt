34e51bbf9cf346b6f0747b9a2eb46ca6aa37bc06
gers <geraquem@gmail.com>
2024-07-22 18:07:20 +0200

    wip

--- app/src/main/java/com/mmfsin/musicmaster/presentation/categories/viewpager/bottomsheet/BSheetSelector.kt
+++ app/src/main/java/com/mmfsin/musicmaster/presentation/categories/viewpager/bottomsheet/BSheetSelector.kt
@@ -1,10 +1,16 @@
 package com.mmfsin.musicmaster.presentation.categories.viewpager.bottomsheet
 
+import android.app.Dialog
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.mmfsin.musicmaster.R
 import com.mmfsin.musicmaster.databinding.BottomSheetSelectorBinding
 import com.mmfsin.musicmaster.presentation.models.GameMode
 import com.mmfsin.musicmaster.presentation.models.GameMode.GUESS_TITLE
@@ -28,6 +34,20 @@ class BSheetSelector(val action: (mode: GameMode) -> Unit) : BottomSheetDialogFr
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setListeners()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog.setOnShowListener { dialogInterface ->
+            val bottomSheetDialog = dialogInterface as BottomSheetDialog
+            val bottomSheet =
+                bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+
+            bottomSheet?.let {
+                it.background = ContextCompat.getDrawable(requireContext(), R.drawable.bg_top_box)
+            }
+        }
+        return dialog
     }
 
     private fun setListeners() {
